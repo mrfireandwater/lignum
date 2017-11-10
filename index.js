0b5d35d3d515ffb417f34bbd3d844312f62f8d68
Loïc <loic.rochat@gmail.com>
2017-11-10 21:00:30 +0100

    test18-post

--- index.js
+++ index.js
@@ -43,10 +43,10 @@ function forwardMessageToFacebook(sender, text) {
 // Route that receives a POST request to /sms
 app.post('/senddata/', function (req, res) {
 	const body = req.body.Body
-	sendTextMessage(loicsender, body.text)
+	sendTextMessage(loicsender, "msg from esp32")
     forwardMessageToFacebook(loicsender, "forwardMessageToFacebook")
     res.set('Content-Type', 'text/plain')
-    res.send('You sent: '+body.text+' to Express')
+    res.send('received')
 
 })
 
